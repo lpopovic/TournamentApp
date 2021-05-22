2ccbc7840affa8923c5753b35029cd8488af6a26
Lazar Popovic <lazar.popovic.riitech.com>
2021-05-22 21:25:46 +0200

    create model Player

---
+++ TournamentApp/TournamentApp/Models/Player.swift
@@ -0,0 +1,16 @@
+//
+//  Player.swift
+//  TournamentApp
+//
+//  Created by MacBook on 5/22/21.
+//
+
+import Foundation
+
+struct Player: Codable {
+    let id: String
+    let firstName: String
+    let lastName: String
+    let points: String
+    let tournament_id: String
+}
