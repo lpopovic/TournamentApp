3c03a4fc9bcbae9aff0f3bc7e7e5ab9a8c49cea3
Lazar Popovic <lazar.popovic@htecgroup.com>
2023-04-06 18:29:10 +0200

    init AppDateFormatter

---
+++ TournamentApp/TournamentApp/Utilities/DateFormat/DateFormat.swift
@@ -0,0 +1,45 @@
+//
+//  AppDateFormatter.swift
+//  TournamentApp
+//
+//  Created by Lazar Popovic on 6.4.23..
+//
+
+import Foundation
+
+enum AppDateFormatter {
+    case reverseShortDate
+    case shortDate
+    case dateAndTime
+    case custom(String)
+    
+    var format: String {
+        switch self {
+        case .reverseShortDate:
+            return "YYYY-MM-DD"
+        case .shortDate:
+            return "dd-MM-YYYY"
+        case .dateAndTime:
+            return "yyyy-MM-dd HH:mm:ss"
+        case .custom(let format):
+            return format
+        }
+    }
+    
+    // MARK: - Properties
+        
+    private var formatter: DateFormatter {
+        let dateFormatter = DateFormatter()
+        return dateFormatter
+    }
+    
+    // MARK: - Public methods
+    
+    func string(from date: Date) -> String {
+        formatter.string(from: date)
+    }
+    
+    func date(from string: String) -> Date? {
+        formatter.date(from: string)
+    }
+}
